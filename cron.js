daa559860d5b13313ff62d2ff02cfd96faeeb0a1
Martin Engström <xealot@gmail.com>
2014-12-08 15:59:44 +0100

    Changed from crontab to node-cron for scheduling; Modified the systemd service to invoke forever instead of tracking by itself

--- cron.js
+++ cron.js
@@ -1,16 +1,20 @@
-/* Run this from a cronjob to clean up old files */
-// * * * * * wget -q --spider http://localhost/tmp/cron.cleanup.php
+/* Run this file as a service like server.js */
+
+var CronJob = require('cron').CronJob;
 var fs = require('fs');
 var filesDir = __dirname + '/files/';
 var maxAge = 30*60*1000;	// 30 minutes;
 
-fs.readdir(filesDir, function(err, files) {
-	files.forEach(function (file) {
-		fs.stat(filesDir + file, function(err, stats) {
-			var age = Date.now() - stats.ctime.getTime();
-			if (age >= maxAge) {
-				fs.unlinkSync(filesDir + file);
-			}
+
+new CronJob('* * * * *', function() {
+	fs.readdir(filesDir, function(err, files) {
+		files.forEach(function (file) {
+			fs.stat(filesDir + file, function(err, stats) {
+				var age = Date.now() - stats.ctime.getTime();
+				if (age >= maxAge) {
+					fs.unlinkSync(filesDir + file);
+				}
+			});
 		});
 	});
-});
+}).start();
